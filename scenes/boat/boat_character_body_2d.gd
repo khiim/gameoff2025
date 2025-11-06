7f216ab49a02f6b758d12c72b821dd8f06e8f1ab
Kristian Hiim <870647+khiim@users.noreply.github.com>
2025-11-06 19:22:54 +0100

    Improve boat physics
    Allow boat to move faster than engine max speed

--- scenes/boat/boat_character_body_2d.gd
+++ scenes/boat/boat_character_body_2d.gd
@@ -1,10 +1,17 @@
 extends CharacterBody2D
 
 @export var acceleration: float = 400.0
-@export var max_speed: float = 600.0
+@export var absolute_max_speed: float = 600.0
+@export var engine_max_speed: float = 600.0
 @export var turn_speed: float = 4.0
 @export var linear_damping: float = 0.98
 @export var lateral_drag: float = 0.2
+
+var _impulse_force: Vector2 = Vector2.ZERO
+
+
+func add_impulse(force: Vector2) -> void:
+	_impulse_force += force
 
 
 func _physics_process(delta: float) -> void:
@@ -17,18 +24,26 @@ func _physics_process(delta: float) -> void:
 	elif Input.is_action_pressed("reverse"):
 		throttle = -0.5
 
-	# Apply forward acceleration
+	# Calculate forward
 	var forward = Vector2.RIGHT.rotated(rotation)
-	velocity += forward * throttle * acceleration * delta
 
-	# Cap speed
-	if velocity.length() > max_speed:
-		velocity = velocity.normalized() * max_speed
+	# Forward speed
+	var forward_speed := velocity.dot(forward)
 
-	# Turn based on speed (no steering if stopped)
-	var speed_factor = clamp(velocity.length() / max_speed, 0.0, 1.0)
-	if velocity.normalized().dot(forward) < 0:
+	# Only accelerate if we are below the engine_max_speed in the direction we are facing
+	if (
+		(throttle > 0 and forward_speed < engine_max_speed)
+		or (throttle < 0 and forward_speed > -engine_max_speed)
+	):
+		velocity += forward * throttle * acceleration * delta
+
+	# Turn based on speed
+	var speed_factor = clamp(abs(forward_speed) / engine_max_speed, 0.2, 1.0)
+
+	# Need to reverse the speed_factor when backing
+	if forward_speed < 0:
 		speed_factor = -speed_factor
+
 	rotation += steer_input * turn_speed * speed_factor * delta
 
 	# Lateral drag — kill sideways motion
@@ -39,4 +54,12 @@ func _physics_process(delta: float) -> void:
 	# Apply damping
 	velocity *= linear_damping
 
-	move_and_slide()
+	# Cap maximum speed
+	if velocity.length() > absolute_max_speed:
+		velocity = velocity.normalized() * absolute_max_speed
+
+	if move_and_slide():
+		velocity = get_real_velocity()
+		#pass
+
+	#print(velocity)
