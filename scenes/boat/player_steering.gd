d229e3eab5b98a52287d97eead4bdc88f049783c
Marius Tendeland Horne <marius_t_h@hotmail.com>
2025-11-13 20:29:16 +0100

    Can spawn waves, with some shaders and crash against other rigid boats

--- scenes/boat/player_steering.gd
+++ scenes/boat/player_steering.gd
@@ -2,9 +2,14 @@ class_name PlayerSteering
 extends Node
 
 @export var boat: Boat
+@export var wave_cooldown_time: float = 2.0
+
+var wave_cooldown: float = 0.0
 
 
 func _physics_process(_delta: float) -> void:
+	if wave_cooldown > 0:
+		wave_cooldown -= _delta
 	# Input
 	var steer_input: float = Input.get_axis("turn_left", "turn_right")
 
@@ -13,6 +18,10 @@ func _physics_process(_delta: float) -> void:
 		throttle = 1.0
 	elif Input.is_action_pressed("reverse"):
 		throttle = -0.5
-
+	
+	if Input.is_action_pressed("wave") and boat and wave_cooldown <= 0:
+		wave_cooldown = wave_cooldown_time
+		boat.spawn_wave()
+	
 	if boat:
 		boat.set_input(steer_input, throttle)
