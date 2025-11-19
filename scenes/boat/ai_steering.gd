df8ed6f06f15f7798d1ef1560b94d3c3ac5fa99c
Marius Tendeland Horne <MariusTH@users.noreply.github.com>
2025-11-19 21:08:25 +0100

    remove unused files and make ai shoot waves (#12)
    * remove unused files and make ai shoot waves
    
    * linting

--- scenes/boat/ai_steering.gd
+++ scenes/boat/ai_steering.gd
@@ -5,7 +5,9 @@ extends NavigationAgent2D
 @export var targets: Array[Node2D]
 
 @export var min_angle_diff: float = 0.5
+@export var wave_cooldown_time: float = 2.0
 
+var wave_cooldown: float = 0.0
 var _target_index: int = 0
 
 
@@ -24,8 +26,8 @@ func steer_toward(target_point: Vector2) -> void:
 
 func _ready() -> void:
 	# Setup NavigationAgent2D properties
-	path_desired_distance = 20
-	target_desired_distance = 150
+	path_desired_distance = 150
+	target_desired_distance = 250
 	path_max_distance = 100
 
 	avoidance_enabled = true
@@ -36,9 +38,15 @@ func _ready() -> void:
 
 
 func _physics_process(_delta: float) -> void:
+	if wave_cooldown > 0:
+		wave_cooldown -= _delta
+
 	if is_navigation_finished():
 		_target_index = (_target_index + 1) % targets.size()
 		target_position = targets[_target_index].global_position
 
 	var next_path_pos := get_next_path_position()
 	steer_toward(next_path_pos)
+	if boat.is_boat_infront() and wave_cooldown <= 0:
+		wave_cooldown = wave_cooldown_time
+		boat.spawn_wave()
