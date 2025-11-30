c48a8fff19a3f64bd4f50c0d143d46b1c594fdce
Marius Tendeland Horne <marius_t_h@hotmail.com>
2025-11-30 20:55:43 +0100

    linting

--- scenes/ui/pause_menu.gd
+++ scenes/ui/pause_menu.gd
@@ -1,12 +1,15 @@
 extends Control
 
+
 func resume():
 	visible = false
 	get_tree().paused = false
 
+
 func pause():
 	visible = true
 	get_tree().paused = true
+
 
 func test_pause():
 	if Input.is_action_just_pressed("pause"):
@@ -19,6 +22,7 @@ func test_pause():
 func _on_resume_pressed():
 	resume()
 
+
 func _on_restart_pressed():
 	get_tree().reload_current_scene()
 
@@ -26,8 +30,10 @@ func _on_restart_pressed():
 func _on_quit_pressed():
 	get_tree().quit()
 
+
 func _process(delta):
 	test_pause()
+
 
 func _ready():
 	visible = false
