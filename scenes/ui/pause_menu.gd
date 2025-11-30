06ef56cc459980218ca4deedba59b9c0f306cc89
Marius Tendeland Horne <marius_t_h@hotmail.com>
2025-11-30 20:53:21 +0100

    Simple pause scene

---
+++ scenes/ui/pause_menu.gd
@@ -0,0 +1,33 @@
+extends Control
+
+func resume():
+	visible = false
+	get_tree().paused = false
+
+func pause():
+	visible = true
+	get_tree().paused = true
+
+func test_pause():
+	if Input.is_action_just_pressed("pause"):
+		if get_tree().paused == false:
+			pause()
+		else:
+			resume()
+
+
+func _on_resume_pressed():
+	resume()
+
+func _on_restart_pressed():
+	get_tree().reload_current_scene()
+
+
+func _on_quit_pressed():
+	get_tree().quit()
+
+func _process(delta):
+	test_pause()
+
+func _ready():
+	visible = false
