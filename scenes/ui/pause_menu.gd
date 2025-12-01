74714bdf2891a5b46ed4fac766f35e9818d1d6c4
Marius Tendeland Horne <marius_t_h@hotmail.com>
2025-12-01 18:25:10 +0100

    linting

--- scenes/ui/pause_menu.gd
+++ scenes/ui/pause_menu.gd
@@ -27,6 +27,7 @@ func _on_restart_pressed():
 	get_tree().paused = false
 	get_tree().reload_current_scene()
 
+
 func _process(_delta):
 	test_pause()
 
@@ -38,4 +39,4 @@ func _ready():
 func _on_main_menu_pressed():
 	# Needs to load main menu
 	# get_tree().change_scene_to_file("res://scenes/world.tscn")
-	pass # Replace with function body.
+	pass  # Replace with function body.
